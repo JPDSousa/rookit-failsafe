2997ec57968e48bdc24fd8f26f847089041288fc
João Sousa <jpd.sousa@campus.fct.unl.pt>
2019-01-11 22:15:39 +0000

    Root failsafe module

---
+++ rookit-failsafe-lib/src/main/java/org/rookit/failsafe/FailsafeModule.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Joao Sousa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+package org.rookit.failsafe;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.google.inject.util.Modules;
+import org.rookit.failsafe.array.ArrayFailsadeModule;
+import org.rookit.failsafe.collection.CollectionFailsafeModule;
+import org.rookit.failsafe.comparable.ComparableFailsafeModule;
+import org.rookit.failsafe.map.MapFailsafeModule;
+import org.rookit.failsafe.number.NumberFailsafeModule;
+import org.rookit.failsafe.object.ObjectFailsafeModule;
+import org.rookit.failsafe.optional.OptionalFailsafeModule;
+import org.rookit.failsafe.string.StringFailsafeModule;
+import org.rookit.failsafe.time.TimeFailsafeModule;
+
+public final class FailsafeModule extends AbstractModule {
+
+    private static final Module MODULE = Modules.combine(
+            new FailsafeModule(),
+            ObjectFailsafeModule.getModule(),
+            MapFailsafeModule.getModule(),
+            OptionalFailsafeModule.getModule(),
+            CollectionFailsafeModule.getModule(),
+            ComparableFailsafeModule.getModule(),
+            ArrayFailsadeModule.getModule(),
+            StringFailsafeModule.getModule(),
+            NumberFailsafeModule.getModule(),
+            TimeFailsafeModule.getModule()
+    );
+
+    public static Module getModule() {
+        return MODULE;
+    }
+
+    private FailsafeModule() {}
+
+    @Override
+    protected void configure() {
+        bind(Failsafe.class).to(BaseFailsafe.class).in(Singleton.class);
+    }
+
+}
